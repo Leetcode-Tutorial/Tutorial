77cbab6723c1e0b7ceab765b8be1176664fa6bb5
Kelukin <z460569416@163.com>
2017-03-09 23:28:37 +0800

    Update Solution.cpp

--- 124-Binary Tree Maximum Path Sum/Solution.cpp
+++ 124-Binary Tree Maximum Path Sum/Solution.cpp
@@ -7,13 +7,13 @@ public:
     }
     int dfs(TreeNode* cur, int& ans)
     {
-        int x = (cur->left)? dfs(cur->left, ans):0;// visit left_son
-        int y = (cur->right)? dfs(cur->right,  ans):0;// visit right_son
-        int z = x + y + cur->val;
-        ans = (z>ans)? z:ans;
+        int x = (cur->left)? dfs(cur->left, ans):0;// 如果存在左儿子，则计算出左儿子的最大单链
+        int y = (cur->right)? dfs(cur->right,  ans):0;//如果存在右儿子，则计算出右儿子的最大单链
+        int z = x + y + cur->val;// 因为若一儿子的最大单链无法造成正贡献则直接取零，所以题解中两种情况可合在一个式子来表示。计算出以当前结点为深度最小的路径的最大值
+        ans = (z>ans)? z:ans;//更新全局的最大值
         x = x+cur->val;
         y = y+cur->val;
-        if(x<y) x = y;
-        return (x>0)? x:0;
+        if(x<y) x = y;//计算出当前节点的最大单链值
+        return (x>0)? x:0;//返回最大单链的值，如果其为负数则无法对父节点造成正的贡献，不如不取，即直接返回0
     }
 };
