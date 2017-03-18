aab62c8a3df57b266490122513a56c0c36cf191f
Sitan Gao <625649135@qq.com>
2017-03-18 17:49:15 +0800

    Update solution.cpp

--- 72-Edit Distance/solution.cpp
+++ 72-Edit Distance/solution.cpp
@@ -15,7 +15,7 @@ int minDistance(string word1, string word2)
     {
         for(int j=1;j<=len2;j++)
         {
-            if(word1[i-1]==word2[j-1])dp[i][j]=min(dp[i-1][j]+1,dp[i][j-1]+1,dp[i-1][j-1]);//两个字符相等
+            if(word1[i-1]==word2[j-1])dp[i][j]=dp[i-1][j-1];//两个字符相等
             else dp[i][j]=min(dp[i-1][j]+1,min(dp[i][j-1]+1,dp[i-1][j-1]+1));//两个字符不相等
         }
     }
