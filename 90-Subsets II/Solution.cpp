1136ca487154c9a62c5c63a352d9056cb511e566
Kelukin <z460569416@163.com>
2017-03-18 17:03:18 +0800

    Update Solution.cpp

--- 90-Subsets II/Solution.cpp
+++ 90-Subsets II/Solution.cpp
@@ -17,8 +17,8 @@ public:
                 V.push_back(tmp);
             }
             V.push_back(vector<int>(1,i->first));//加入单独一个整数x构成的子集
-            int r = V.size();//通过l与r标识元素x出现j次的子集在vector内存储的区间
-            for(int j=1;j<cnt;j++)
+            int r = V.size();//通过l与r标识元素x出现(j-1)次的子集在vector内存储的区间
+            for(int j=2;j<=cnt;j++)//当前整数x第2次到第cnt次出现时，将 V 中 l 与 r 指出的集合区间的每个元素都与 x 组合
             {
                 for(int k=l;k<r;k++)
                 {
