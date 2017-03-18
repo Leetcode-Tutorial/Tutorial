25c97806b66de0f4e1dce4337e6527ff4949cf18
Kelukin <z460569416@163.com>
2017-03-18 17:00:34 +0800

    Update Solution.cpp

--- 90-Subsets II/Solution.cpp
+++ 90-Subsets II/Solution.cpp
@@ -10,14 +10,14 @@ public:
         {
             int cnt = M[i->first];
             int l = V.size();
-            for(int k=0;k<l;k++)//若当前整数x第一次出现,将当前集合V的每个元素都与x组合
+            for(int k=0;k<l;k++)//特殊处理整数x第一次出现出现的情况,将当前集合V的每个元素都与x组合
             {
                 vector<int> tmp = V[k];
                 tmp.push_back(i->first);
                 V.push_back(tmp);
             }
-            V.push_back(vector<int>(1,i->first));
-            int r = V.size();//通过l与r标识元素x出现n次的子集在vector内存储的区间
+            V.push_back(vector<int>(1,i->first));//加入单独一个整数x构成的子集
+            int r = V.size();//通过l与r标识元素x出现j次的子集在vector内存储的区间
             for(int j=1;j<cnt;j++)
             {
                 for(int k=l;k<r;k++)
