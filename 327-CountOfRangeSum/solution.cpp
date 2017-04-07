8441869725d7d7b81a86a4e608027004fe9cead5
Haibin Xie <lambdax.tyler@gmail.com>
2017-04-07 19:23:01 +0800

    Update solution.cpp

--- 327-CountOfRangeSum/solution.cpp
+++ 327-CountOfRangeSum/solution.cpp
@@ -3,24 +3,24 @@ class Solution
 public:
     int mergeSort(vector<long long> &sum,int L,int R,int lower,int upper)
     {
-        if(L==R)    return 0;
+        if(R-L<=1)    return 0;
         int mid=(L+R)/2;
-        int count=mergeSort(sum,L,mid,lower,upper)+mergeSort(sum,mid+1,R,lower,upper);
-        int lp=mid+1,rp=mid+1;
-        for(int i=L;i<=mid;i++)
+        int count=mergeSort(sum,L,mid,lower,upper)+mergeSort(sum,mid,R,lower,upper);
+        int lp=mid,rp=mid;
+        for(int i=L;i<mid;i++)
         {
-            while(lp<=R&&sum[lp]-sum[i]<lower)    ++lp;
-            while(rp<=R&&sum[rp]-sum[i]<=upper)   ++rp;
+            while(lp<R&&sum[lp]-sum[i]<lower)    ++lp;
+            while(rp<R&&sum[rp]-sum[i]<=upper)   ++rp;
             count+=rp-lp;
         }
-        inplace_merge(sum.begin()+L,sum.begin()+mid,sum.begin()+R+1);
+        inplace_merge(sum.begin()+L,sum.begin()+mid,sum.begin()+R);
         return count;
     }
     int countRangeSum(vector<int>& nums, int lower, int upper)
     {
         int n=nums.size()+1;
         vector<long long> sum(n,0);
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<n;i++)
             sum[i]=sum[i-1]+nums[i-1];
         return mergeSort(sum,0,n,lower,upper);
     }
