61ff0ccb1475d1ecea845c31321b6bd81926a061
601442991 <601442991@qq.com>
2017-04-14 18:33:58 +0800

    solution for 330, 538

---
+++ 538-ConvertBSTtoGreaterTree/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    TreeNode* convertBST(TreeNode* root) {
+        if(root){
+            convertBST(root->right);
+            root->val += sum;
+			sum = root->val
+            convertBST(root->left);
+        }
+        return root;
+    }
+};
