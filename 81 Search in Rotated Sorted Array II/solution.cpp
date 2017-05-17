ace3d2ee5afc3217c7301e1fd4d049dd39735ca6
Libo Qin <1743625930@qq.com>
2017-05-17 19:43:05 +0800

    solution for 520,557 (#221)
    * solution for 102 103
    
    * rename the name
    
    * add a )
    
    * modify the format
    
    * add 75,349
    
    * update 349
    
    * add solution for 78,79
    
    * update 79
    
    * add solution for 414,71
    
    * add solution for 81,189
    
    * add solution for 474,7
    
    * add solution
    
    * add 520.557
    
    * add 520,557

--- 81 Search in Rotated Sorted Array II/solution.cpp
+++ 81 Search in Rotated Sorted Array II/solution.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-bool search(vector<int>& nums, int target) {
-//扫描一遍肯定可以，那么放在这里不是这么简单的,是希望我们用log级别的
-//方法一我们先用师兄的做法，先二分找到那个边界，然后分开进行二分查找即可~
-//方法二用下面这种方法
-
-int len = nums.size();
-int left = 0,right = len-1;
-while(left<=right)
-{
-int mid = (left+right)/2;
-if(nums[mid] == target)
-{
-return true;
-}
-if(nums[mid]==nums[left])
-left++;
-else if(nums[mid]>=nums[left]) //砍掉的一半一定要是可以砍掉的
-{
-if(target<=nums[mid]&&target>=nums[left])
-right = mid-1;
-else
-left = mid+1;
-
-}
-else
-{
-if(nums[mid]< target&&target<=nums[right])
-left = mid+1;
-else
-right = mid-1;
-}
-}
-return false;
-}
+    bool search(vector<int>& nums, int target) {
+        //扫描一遍肯定可以，那么放在这里不是这么简单的,是希望我们用log级别的
+        //方法一我们先用师兄的做法，先二分找到那个边界，然后分开进行二分查找即可~
+        //方法二用下面这种方法
+        
+        int len = nums.size();
+        int left = 0,right = len-1;
+        while(left<=right)
+        {
+            int mid = (left+right)/2;
+            if(nums[mid] == target)
+            {
+                return true;
+            }
+            if(nums[mid]==nums[left])
+                left++;
+            else if(nums[mid]>nums[left])        //砍掉的一半一定要是可以砍掉的
+                {
+                    if(target<=nums[mid]&&target>=nums[left])
+                        right = mid-1;
+                    else
+                        left = mid+1;
+                    
+                }
+            else
+            {
+                if(nums[mid]< target&&target<=nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+        return false;
+    }
 };
